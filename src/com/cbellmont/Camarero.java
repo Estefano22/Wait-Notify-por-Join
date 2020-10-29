b22afc0ae6bee6e414fc99aeb81f007cba76f426
estefano <sandulescustefano8@gmail.com>
2020-10-29 20:50:00 +0100

    Hecho

--- src/com/cbellmont/Camarero.java
+++ src/com/cbellmont/Camarero.java
@@ -6,7 +6,7 @@ public class Camarero extends Thread {
     public void run() {
         try {
             while (true) {
-            }
+                {
             prepararComanda();
                 }
             }
--- src/com/cbellmont/Cliente.java
+++ src/com/cbellmont/Cliente.java
@@ -3,23 +3,22 @@ package com.cbellmont;
 public class Cliente extends Thread {
 camarero camarero;
 
-public Cliente (Camarero Camarero){
+public Cliente (Camarero camarero){
     this.camarero=camarero;
 }
     @Override
     public void run() {
         try {
             while (true) {
-
                     if (Main.list.isEmpty()){
-                        camarero.prepararComanda();
+                        camarero.preparaComanda();
                         System.out.println("Cliente - Voy a esperar a que me traigan la comida");
 
 
 
                     } else {
                         System.out.println("Cliente - Voy a a consumir un \" + Main.list.get(0) + \" 3 segundos");
-                        Main.list.remove();
+                        Main.list.remove(0);
                         sleep(2000);
                     }
                 }
